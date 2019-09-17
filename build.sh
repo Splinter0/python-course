3b06813c45c199cdf46373186b3510d3000304fa
Jyry Hjelt <hjelt.jyry@gmail.com>
2019-09-17 13:39:02 +0200

    fixed build script

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@ for lecture in $(ls src)
 do 
 	echo "building '$lecture'..."
 	mkdir pdfs/$lecture
-	
-	for file in $(ls src/$lecture *.md)
-	do
-		pandoc src/$lecture/$file -o pdfs/$lecture/$(basename $file .md).pdf
-	done
+
+done
+
+for file in $(find src | grep .md)
+do
+	echo $file
+	pandoc $file -o pdfs/$(echo $file | cut -d "/" -f 2-3 | cut -d "." -f 1).pdf
 done
 
